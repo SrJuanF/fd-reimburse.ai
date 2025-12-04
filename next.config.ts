ad08aea9ae8871686b9304e33aa77287c381904f
Felipe Giraldo <juinfe2030@gmail.com>
2025-12-04 14:16:19 -0500

    Deploy

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,23 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   transpilePackages: ['geist'],
+  poweredByHeader: false,
+  productionBrowserSourceMaps: false,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         hostname: 'vercel.com',
       },
     ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
