c9d10fb4401eaf4e74f17ec072257ce703b15321
Shunsuke0401 <snakatani0401@gmail.com>
2025-12-07 15:27:21 -0800

    whitelist updated

--- lib/whitelist.ts
+++ lib/whitelist.ts
@@ -1,6 +1,7 @@
 export const ALLOWED_WALLETS: string[] = [
   //Mack
   "0x5D262Ad5F60189Bb21Eb6cF6BCA7Db04F2C01518".toLowerCase(),
+  "0x14ba2ea22a27a771fb2036db3c05bc04d0439d30".toLowerCase(),
   //Juan
   "0x12745b393D63072ba400d943Ce9657f82C7f4522".toLowerCase(),
   "0x7b2504bA87E36ed194c3EAcA55f3efc5f44fEcF0".toLowerCase(),
